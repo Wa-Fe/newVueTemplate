39fbe6353090f810dbd423a2ee31199eba2c9000
long <wa.fe@foxmail.com>
2024-11-18 10:49:19 +0800

    feat: 暂存

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <HelloWorld msg="Vite + Vue" />
+    <Button>dsad</Button>
+  </div>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue"
+</script>
+
+<style scoped lang="scss"></style>
